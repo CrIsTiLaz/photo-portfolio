bd40cca8e43ecb1eec5e4e5644f25e2b3d31bf4a
Lazea Cristian <cristian.lazea01@e-uvt.ro>
2024-08-25 22:17:25 +0300

    About section done

--- src/app/(components)/about/page.jsx
+++ src/app/(components)/about/page.jsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function page() {
-  return <section className="section bg-yellow-200">About</section>;
+  return (
+    <section className="section">
+      <div className="container mx-auto h-full relative">
+        <div
+          className=" flex flex-col lg:flex-row h-full 
+        items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16"
+        >
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <Image src="/man.png" alt="man" width={1000} height={1000} />
+          </div>
+          <div className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start">
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <b>Doloribus, recusandae reiciendis, </b>
+              mollitia ratione voluptatem placeat sint adipisci beatae rerum
+              vero sed
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              delectus ipsum pariatur fuga qui libero eligendi architecto quas
+              aliquam omnis?
+            </p>
+
+            <Link href={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default page;
